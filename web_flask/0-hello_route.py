de25d08916ceeba0b6967eb0b6509c1440a5ee3a
Abdiel-88 <abdiel.f@outlook.com>
2024-04-09 20:43:51 -0400

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,15 @@
+#!/usr/bin/python3
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+# Ensuring strict_slashes are False for all routes
+app.url_map.strict_slashes = False
+
+@app.route('/')
 def hello_hbnb():
-    return "<p>Hello HBNB!</p>"
+    """Returns 'Hello HBNB!' to the client."""
+    return 'Hello HBNB!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
