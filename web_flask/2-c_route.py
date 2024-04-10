208088575f3107e3e1e78a72a4ae051a94e1cf1e
Abdiel-88 <abdiel.f@outlook.com>
2024-04-10 11:23:25 -0400

    fix task 2 and pycodestyle

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-from flask import Flask
+"""Start a Flask web application with dynamic route"""
 
+from flask import Flask
 app = Flask(__name__)
 
-# Flask automatically considers strict_slashes to be True; setting it to False globally.
+# Setting strict_slashes to False globally
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def hello_hbnb():
-    """Returns 'Hello HBNB!' to the client."""
+    """Return 'Hello HBNB!' for the root route"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
-    """Returns 'HBNB' to the client."""
+    """Return 'HBNB' for the /hbnb route"""
     return 'HBNB'
 
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """Returns 'C ' followed by the value of the text variable."""
-    return 'C {}'.format(text.replace('_', ' '))
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@app.route('/c/<text>')
+def c_text(text):
+    """Return 'C <text>', and replace underscore with space in <text>"""
+    text_no_underscore = text.replace('_', ' ')
+    return 'C {}'.format(text_no_underscore)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
