f3bd220963bcc24d93699ba5b4082cc902e8591b
Abdiel-88 <abdiel.f@outlook.com>
2024-04-10 11:59:24 -0400

    task 8 fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-"""Starts a Flask web application with listing of states"""
+"""Start web application with two routings
+"""
 
-from flask import Flask, render_template
 from models import storage
+from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
-
-app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
 def states_list():
+    """Render template with states
     """
-    Displays an HTML page with a list of
-    all State objects present in DBStorage.
-    """
-    states = storage.all("State").values()
-    return render_template('7-states_list.html', states=states)
+    path = '7-states_list.html'
+    states = storage.all(State)
+    # sort State object alphabetically by name
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Remove the current SQLAlchemy Session."""
+def app_teardown(arg=None):
+    """Clean-up session
+    """
     storage.close()
 
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
