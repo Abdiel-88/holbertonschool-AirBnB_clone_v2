c0922822e1a8ecdfa7b3b75e6e423ebc54b3598a
Abdiel-88 <abdiel.f@outlook.com>
2024-04-10 11:20:08 -0400

    fix task 1 and pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-from flask import Flask
+"""Start a Flask web application with multiple routes"""
 
+from flask import Flask
 app = Flask(__name__)
 
-# Ensure strict_slashes are set to False for all routes
+# Setting strict_slashes to False globally
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def hello_hbnb():
-    """Returns 'Hello HBNB!' to the client."""
+    """Return 'Hello HBNB!' for the root route"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
-    """Returns 'HBNB' to the client."""
+    """Return 'HBNB' for the /hbnb route"""
     return 'HBNB'
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
