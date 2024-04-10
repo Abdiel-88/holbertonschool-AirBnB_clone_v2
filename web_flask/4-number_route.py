b27d8668bb43bcf78cc20eced151ca3af06927c7
Abdiel-88 <abdiel.f@outlook.com>
2024-04-10 11:34:06 -0400

    task 4 fix again

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,38 +1,47 @@
 #!/usr/bin/python3
-"""Start a Flask web application with more complex dynamic routes"""
+"""Start web application with two routings
+"""
 
 from flask import Flask
 app = Flask(__name__)
 
-# Enforce strict_slashes=False globally
-app.url_map.strict_slashes = False
-
 
 @app.route('/')
-def hello_hbnb():
-    """Return 'Hello HBNB!'"""
+def hello():
+    """Return string when route queried
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """Return 'HBNB'"""
+    """Return string when route queried
+    """
     return 'HBNB'
 
 
 @app.route('/c/<text>')
-def c_text(text):
-    """Display 'C <text>', with underscores replaced by spaces"""
+def c_is_fun(text):
+    """Return reformatted text
+    """
     return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/')
 @app.route('/python/<text>')
-def python_text(text):
-    """Display 'Python <text>', with underscores replaced by spaces.
-    The default value of text is 'is cool'."""
+def python_with_text(text='is cool'):
+    """Reformat text based on optional variable
+    """
     return 'Python ' + text.replace('_', ' ')
 
 
+@app.route('/number/<int:n>')
+def number(n=None):
+    """Allow request if path variable is a valid integer
+    """
+    return str(n) + ' is a number'
+
+
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
