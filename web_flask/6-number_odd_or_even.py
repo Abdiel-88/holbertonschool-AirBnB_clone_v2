d99d79f160d9d0f8cebbd0722d94c5d7c2daa58b
Abdiel-88 <abdiel.f@outlook.com>
2024-04-10 11:48:19 -0400

    fix task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,38 +1,48 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+"""Starts a Flask web application with more dynamic routing and templates"""
 
+from flask import Flask, render_template
 app = Flask(__name__)
+
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
-def hello_hbnb():
+def hello():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def c_is_fun(text):
-    return 'C {}'.format(text.replace('_', ' '))
+    return 'C ' + text.replace('_', ' ')
 
-@app.route('/python/')
+
+@app.route('/python/', defaults={'text': 'is cool'})
 @app.route('/python/<text>')
-def python_is_cool(text="is cool"):
-    return 'Python {}'.format(text.replace('_', ' '))
+def python_with_text(text):
+    return 'Python ' + text.replace('_', ' ')
+
 
 @app.route('/number/<int:n>')
 def number(n):
-    return '{} is a number'.format(n)
+    return str(n) + ' is a number'
+
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
     return render_template('5-number.html', n=n)
+
 
 @app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
     parity = "even" if n % 2 == 0 else "odd"
     return render_template('6-number_odd_or_even.html', n=n, parity=parity)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
