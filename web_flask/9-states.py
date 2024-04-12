1a8c66d84459a6012b05b0137b11e09477e78d6c
Abdiel-88 <abdiel.f@outlook.com>
2024-04-12 11:40:56 -0400

    task 10 fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
+"""Start web application with two routings"""
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/states')
+@app.route('/states', defaults={'id': None})
 @app.route('/states/<id>')
-def states_list(id=None):
-    """Render template with states
+def states_list(id):
     """
-    path = '9-states.html'
-    states = storage.all(State)
-    return render_template(path, states=states, id=id)
+    Render template with states or
+    specific state with cities if id is provided
+    """
+    states = storage.all(State).values()
+    if id:
+        state = next((s for s in states if s.id == id), None)
+        return render_template('9-state.html', state=state)
+    else:
+        sorted_states = sorted(states, key=lambda state: state.name)
+        return render_template('9-states.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
+def teardown_db(exception):
+    """Closes the SQLAlchmey session."""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
