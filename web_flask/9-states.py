37068e71796de39de029e66e36481eb7dbac6c85
Abdiel-88 <abdiel.f@outlook.com>
2024-04-12 17:45:10 -0400

    task 10 fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,41 +1,50 @@
 #!/usr/bin/python3
-"""Start web application with two routings"""
+'''A simple Flask web application.
+'''
+from flask import Flask, render_template
 
-from flask import Flask, render_template, abort
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
+'''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
 
-@app.route('/states', defaults={'id': None})
+@app.route('/states')
 @app.route('/states/<id>')
-def states_list(id):
-    """
-    Render template with states or a
-    specific state with cities if id is provided.
-    """
-    if id:
-        state = storage.all(State).get('State.' + id)
-        if state:
-            # Sort cities of the state by name if using DBStorage
-            cities = sorted(state.cities, key=lambda city: city.name) if hasattr(state, 'cities') else []
-            return render_template('9-state.html', state=state, cities=cities)
-        else:
-            abort(404, description="Not found")
+def states(id=None):
+    '''The states page.'''
+    states = None
+    state = None
+    all_states = list(storage.all(State).values())
+    case = 404
+    if id is not None:
+        res = list(filter(lambda x: x.id == id, all_states))
+        if len(res) > 0:
+            state = res[0]
+            state.cities.sort(key=lambda x: x.name)
+            case = 2
     else:
-        states = storage.all(State).values()
-        # Sort states by name
-        sorted_states = sorted(states, key=lambda state: state.name)
-        return render_template('9-states.html', states=sorted_states)
+        states = all_states
+        for state in states:
+            state.cities.sort(key=lambda x: x.name)
+        states.sort(key=lambda x: x.name)
+        case = 1
+    ctxt = {
+        'states': states,
+        'state': state,
+        'case': case
+    }
+    return render_template('9-states.html', **ctxt)
 
 
 @app.teardown_appcontext
-def close_storage(exception):
-    """Closes the SQLAlchmey session or FileStorage."""
+def flask_teardown(exc):
+    '''The Flask app/request context end event listener.'''
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
