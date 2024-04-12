10954505c18fe043f4713001bcf7bbdd97ff411f
Abdiel-88 <abdiel.f@outlook.com>
2024-04-12 12:59:40 -0400

    task 10 fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Start web application with two routings"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from models import storage
 from models.state import State
 
@@ -13,21 +13,27 @@ app.url_map.strict_slashes = False
 @app.route('/states/<id>')
 def states_list(id):
     """
-    Render template with states or
-    specific state with cities if id is provided
+    Render template with states or a
+    specific state with cities if id is provided.
     """
-    states = storage.all(State).values()
     if id:
-        state = next((s for s in states if s.id == id), None)
-        return render_template('9-state.html', state=state)
+        state = storage.all(State).get('State.' + id)
+        if state:
+            # Sort cities of the state by name if using DBStorage
+            cities = sorted(state.cities, key=lambda city: city.name) if hasattr(state, 'cities') else []
+            return render_template('9-state.html', state=state, cities=cities)
+        else:
+            abort(404, description="Not found")
     else:
+        states = storage.all(State).values()
+        # Sort states by name
         sorted_states = sorted(states, key=lambda state: state.name)
         return render_template('9-states.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the SQLAlchmey session."""
+def close_storage(exception):
+    """Closes the SQLAlchmey session or FileStorage."""
     storage.close()
 
 
